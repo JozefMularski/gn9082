58a908f23f016baa221912784525ff18ac7ada80
Jozef Mularski <jozeffmularski@gmail.com>
2020-03-08 21:29:41 -0400

    added welcome message

--- src/introduction/HelloJava.java
+++ src/introduction/HelloJava.java
@@ -4,7 +4,8 @@ public class HelloJava
 {
 	
 	public static void main(String[] args) 
-	{           
+	{        
+		System.out.println("Welcome");  
 		System.out.print("Jozef Mularski");  
 	}   
 	
